b60aa042c1faf11be569368dbb3df5dbd4ce7fb2
tanelam <tania.e.aparicio@gmail.com>
2018-03-08 17:38:12 -0500

    added delete button and fucntionality

--- src/note.js
+++ src/note.js
@@ -1,33 +1,35 @@
 class Note {
   constructor(data) {
-    this.id = data.id;
-    this.title = data.title;
-    this.body = data.body;
-    // Note.all.push(this);
     this.noteList = document.getElementById("notes-list")
     this.showingNote = document.getElementById("third-column")
+
+      this.id = data.id;
+      this.title = data.title;
+      this.body = data.body;
+
+
   }
 
   renderNoteList(json){
     for(let i = 0; i < json.length; i++){
         // console.log(json[i].id)
     let note = json[i]
+    // console.log(json[0])
 
     let div = document.createElement('div')
     // div.setAttribute("data-noteId", note.id )
     div.setAttribute("class", "notes-container")
     let header = document.createElement("p")
-    header.setAttribute("data-noteId", note.id )
-    let li = document.createElement("li")
-    li.innerText = note.body
+    header.setAttribute("data-noteId", note.id)
+    header.setAttribute("id", note.id)
+    // let li = document.createElement("li")
+    // li.innerText = note.body
     header.innerText = note.title
 
     div.appendChild(header)
     this.noteList.appendChild(div)
 
     header.addEventListener("click", () => {
-      // console.log("mouse check")
-
       this.getNote(event)
 
     })
@@ -40,14 +42,14 @@ class Note {
 
     fetch(`http://localhost:3000/api/v1/notes/${noteId}`)
     .then(resp => resp.json())
-    .then(json => console.log(json))
+    .then(json => this.showNote(json))
   }
-  //
-  // showNote(json){
-  //   let newShow = new Show(json)
-  //   newShow.render();
-  //   // console.log(this.showingNote)
-  // }
+
+  showNote(json){
+    let newShow = new Show(json)
+    newShow.render();
+    // console.log(this.showingNote)
+  }
 
 
 
--- src/show.js
+++ src/show.js
@@ -3,16 +3,21 @@ class Show {
     console.log(data);
     this.noteList = document.getElementById("notes-list")
     this.showingNote = document.getElementById("third-column")
-    // this.title = data.title;
-    // this.id = data.id;
-    // this.user = data.user;
-    // this.body = data.body;
+    // debugger
+
+    if (data){
+      this.title = data.title;
+      this.id = data.id;
+      this.user = data.user;
+      this.body = data.body;
+    }
   }
 
 
   renderForm(){
     // let body = document.body
     let formDiv = document.getElementById("third-column")
+    formDiv.innerText = ""
     let div = document.createElement("div")
     formDiv.append(div)
 
@@ -26,29 +31,30 @@ class Show {
     submit.setAttribute("type", "submit")
     submit.setAttribute("class", "submit")
 
-    let inputContent = document.createElement("textarea")
-    inputContent.setAttribute("cols", "150")
-    inputContent.setAttribute("placeholder", "Title")
-    inputContent.setAttribute("id", "title")
+    let inputTitle = document.createElement("textarea")
+    inputTitle.setAttribute("cols", "120")
+    inputTitle.setAttribute("placeholder", "Title your note")
+    inputTitle.setAttribute("id", "title")
 
     let inputBody = document.createElement("textarea")
-    inputBody.setAttribute("placeholder", "Content")
+    inputBody.setAttribute("placeholder", "Start typing...")
     inputBody.setAttribute("rows", "43")
-    inputBody.setAttribute("cols", "150")
+    inputBody.setAttribute("cols", "120")
     inputBody.setAttribute("id", "body")
 
     form.append(submit)
-    form.append(inputContent)
+    form.append(inputTitle)
     form.append(inputBody)
-    // form.append(submit)
     div.append(form)
     formDiv.append(div)
 
     this.submitButtonEvent()
+
   }
   //
   render(){
     let formDiv = document.getElementById("third-column")
+    formDiv.innerText = ""
     let div = document.createElement("div")
     formDiv.append(div)
 
@@ -62,30 +68,37 @@ class Show {
     submit.setAttribute("type", "submit")
     submit.setAttribute("class", "submit")
 
-    let inputContent = document.createElement("textarea")
-    inputContent.setAttribute("cols", "150")
-    inputContent.setAttribute("placeholder", "Title")
-    inputContent.setAttribute("id", "title")
-    inputContent.innerText = this.title
+    let deleteButton = document.createElement("button")
+    deleteButton.setAttribute("id", "delete-button")
+    deleteButton.innerText = "Delete"
+
+    let inputTitle = document.createElement("textarea")
+    inputTitle.setAttribute("cols", "120")
+    inputTitle.setAttribute("placeholder", "Title your note")
+    inputTitle.setAttribute("id", "title")
+    inputTitle.innerText = this.title
 
     let inputBody = document.createElement("textarea")
-    inputBody.setAttribute("placeholder", "Content")
+    inputBody.setAttribute("placeholder", "Start typing...")
     inputBody.setAttribute("rows", "43")
-    inputBody.setAttribute("cols", "150")
+    inputBody.setAttribute("cols", "120")
     inputBody.setAttribute("id", "body")
     inputBody.innerText = this.body
 
 
     form.append(submit)
-    form.append(inputContent)
+    form.append(deleteButton)
+    form.append(inputTitle)
     form.append(inputBody)
     // form.append(submit)
 
     div.append(form)
     formDiv.append(div)
 
+    // debugger
+    this.editButtonEvent(this.id)
+    this.deleteButtonEvent(this.id)
 
-    this.submitButtonEvent()
 
   }
 
@@ -99,13 +112,12 @@ class Show {
        // console.log("hello")
        let title = document.getElementById("title").value
        let body = document.getElementById("body").value
-        console.log(title)
+        // console.log(title)
         // debugger
 
        this.postNote(title, body)
 
-       document.getElementById("title").value = ""
-       document.getElementById("body").value = ""
+       document.getElementById("form").innerHTML = ""
        })
     }
   }
@@ -124,13 +136,83 @@ class Show {
   }
 
   displayNote(json){
-    // console.log(json)
+     // console.log(json.id)
     let noteList = document.getElementById("notes-list")
-    let li = document.createElement("li")
-    let header = document.createElement('h3')
+
+    let div = document.createElement('div')
+    div.setAttribute("class", "notes-container")
+    let header = document.createElement("p")
+    header.setAttribute("data-noteId", json.id)
+
     header.innerText = json.title
 
-    li.appendChild(header)
-    noteList.appendChild(li)
+    div.appendChild(header)
+    noteList.appendChild(div)
   }
+
+  editButtonEvent(id){
+    let submitButtons = document.getElementsByClassName("submit")
+    for (let i = 0; i < submitButtons.length; i++){
+    submitButtons[i].addEventListener("click", () => {
+      event.preventDefault()
+
+       let title = document.getElementById("title").value
+       let body = document.getElementById("body").value
+
+        let updateNote = document.getElementById(this.id)
+        updateNote.innerText = title
+
+       this.patchNote(title, body)
+
+       // document.getElementById("title").value = ""
+       // document.getElementById("body").value = ""
+       document.getElementById("form").innerHTML = ""
+
+       })
+    }
+  }
+
+  deleteButtonEvent(id){
+    let deleteButton = document.getElementById("delete-button")
+    // console.log(id)
+    deleteButton.addEventListener("click", () => {
+      let updateNote = document.getElementById(this.id)
+      updateNote.innerText = ""
+      console.log(this.id)
+      event.preventDefault()
+      this.deleteButtonFetch()
+
+      document.getElementById("form").innerHTML = ""
+    })
+  }
+
+  deleteButtonFetch(){
+    let noteId = this.id
+    fetch(`http://localhost:3000/api/v1/notes/${noteId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({noteId: noteId})
+    })
+    .then(resp => resp.json())
+    .then(json => alert(json.message))
+  }
+
+  patchNote(title, body){
+    let noteId = this.id
+
+    fetch(`http://localhost:3000/api/v1/notes/${noteId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({title: title, body: body, id: noteId})
+    })
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+  }
+
 }
